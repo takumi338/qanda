bbaa15cb05f9d45e4a0e1cf37460204ed69be4df
takumi338 <53427711+takumi338@users.noreply.github.com>
2020-10-16 15:40:13 +0900

    リレーション設定

--- backend/app/User.php
+++ backend/app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany('App\Post');
+    }
 }
